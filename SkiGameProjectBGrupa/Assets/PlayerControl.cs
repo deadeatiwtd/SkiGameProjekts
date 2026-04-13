d03943c70e140b6a5393a19081ea82c0815775de
deadeatiwtd <erik.ulanov@gmail.com>
2026-04-13 15:44:08 +0300

    1lecturecomplete!

---
+++ SkiGameProjectBGrupa/Assets/PlayerControl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private InputAction move;
+    [SerializeField] private float rotSpeed = 30, speed = 20;
+    private Rigidbody rb;
+    void Awake()
+    {
+        move = InputSystem.actions.FindAction("Player/Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+ 
+    void FixedUpdate()
+    {
+
+        Vector2 moveInput = move.ReadValue<Vector2>();
+
+        transform.Rotate(0, -moveInput.x * rotSpeed * Time.fixedDeltaTime, 0);
+
+        float turnAngle = Mathf.Abs(transform.localEulerAngles.y);
+
+        float speedMult = Mathf.Cos(turnAngle * Mathf.Deg2Rad);
+        
+        rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
+        
+        Debug.Log("turn angle: " + turnAngle);
+    }
+    
+
+    
+}
